3ba4f6cffe64a29adfcaf0e6e5890d33bd68a5d8
Muhammed Furkan GÜNEŞ <themfg.63@gmail.com>
2024-08-06 15:46:03 +0300

    auth component completed

---
+++ src/component/auth/RegisterPage.jsx
@@ -0,0 +1,94 @@
+import React, { useState } from 'react';
+import ApiService from '../../service/ApiService';
+import { useNavigate } from 'react-router-dom';
+
+function RegisterPage() {
+    const navigate = useNavigate();
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: '',
+        phoneNumber: ''
+    });
+
+    const [errorMessage, setErrorMessage] = useState('');
+    const [successMessage, setSuccessMessage] = useState('');
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
+
+    const validateForm = () => {
+        const { name, email, password, phoneNumber } = formData;
+        if (!name || !email || !password || !phoneNumber) {
+            return false;
+        }
+        return true;
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!validateForm()) {
+            setErrorMessage('Lütfen tüm boşlukları doldurunuz.');
+            setTimeout(() => setErrorMessage(''), 5000);
+            return;
+        }
+        try {
+            
+            const response = await ApiService.registerUser(formData);
+
+        
+            if (response.statusCode === 200) {
+                setFormData({
+                    name: '',
+                    email: '',
+                    password: '',
+                    phoneNumber: ''
+                });
+                setSuccessMessage('Kayıt Başarıyla Tamamlandı');
+                setTimeout(() => {
+                    setSuccessMessage('');
+                    navigate('/');
+                }, 3000);
+            }
+        }
+         catch (error) {
+            setErrorMessage(error.response?.data?.message || error.message);
+            setTimeout(() => setErrorMessage(''), 5000);
+        }
+    };
+
+    return (
+        <div className="auth-container">
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+        {successMessage && <p className="success-message">{successMessage}</p>}
+            <h2>Kayıt Ol</h2>
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label>Ad Soyad:</label>
+                    <input type="text" name="name" value={formData.name} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Email:</label>
+                    <input type="email" name="email" value={formData.email} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Telefon No:</label>
+                    <input type="text" name="phoneNumber" value={formData.phoneNumber} onChange={handleInputChange} required />
+                </div>
+                <div className="form-group">
+                    <label>Şifre:</label>
+                    <input type="password" name="password" value={formData.password} onChange={handleInputChange} required />
+                </div>
+                <button type="submit">Kayıt Ol</button>
+            </form>
+            <p className="register-link">
+                Zaten Bir Hesabınız Var Mı?  <a href="/login">Giriş Yap</a>
+            </p>
+        </div>
+    );
+}
+
+export default RegisterPage;
